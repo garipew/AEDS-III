d9254d2a3a184c7ec17816323c7eb894f9bc1ae0
Gabriel Sá <gabriel10043@live.com>
2023-04-23 21:44:18 -0300

    reading file now, rearranged stuff

--- Tp2/main.c
+++ Tp2/main.c
@@ -1,4 +1,4 @@
-#include "grafo.h"
+#include "grafoV2.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <getopt.h>
@@ -7,42 +7,40 @@
 
 void main(int argc, char* argv[]){
 
-    int sol, R, C, testes, valor;
+    int sol, R, C, testes;
     FILE* e;
     Grafo* g;
+    char linha[200];
 
     if(argc < 3){
-        printf("Esperado argumentos <int> <filename>.\n");
+        printf("Esperado argumentos <int> <arquivo.txt>.\n");
         exit(-1);
     }
 
     sol = atoi(argv[1]);
 
     e = fopen(argv[2], "r");
+
     if(e == NULL){
     printf("Falha ao ler o arquivo \"%s\".\n", argv[2]);
     }
 
-    //printf("solucao %d\nentrada: %s\n", sol, argv[2]);
-
     fscanf(e, "%d", &testes);
     for(int i = 0; i < testes; i++){
-
-        fscanf(e, "%d %d", &R, &C);
+        
         g = criaGrafo();
 
-        for(int j = 0; j < R; j++){
-            
-            fscanf(e, "%d", &valor);
-            insereVertice(criaVertice(valor), g);
-            insereAresta(g, j+1, 1, j+2);
+        printf("caso teste %d\n", i+1);
+        fscanf(e, "%d %d", &R, &C);
 
-        }
+        printf("Ler %d x %d números.\n", R, C);
+        fgetc(e);
+        
+        coletaDados(g, R, C, e);
 
-        imprimeArestas(g->primeiro->prox->prox);
-
+        desenhaGrafo(g);
         apagaGrafo(g);
-
+        printf("\n\n");
     }
 
 
